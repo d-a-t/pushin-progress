f1cf4edef321da0dd791d71636f2cd8f14be177c
ziem9243 <152050307+ziem9243@users.noreply.github.com>
2025-11-09 21:41:12 -0500

    Menu Buttons added + random name generator
    Semi completed menu. You can now add to your skill set. You will get a completely random name and family in your relationships tab.

--- sketch.js
+++ sketch.js
@@ -1,36 +1,324 @@
-let age;
-let money;
-let health;
-let looks;
-let intelligence;
+// tables for various decisions
+let people;
+let activities;
+let jobs;
+let education;
+let homes;
+
+// lists for various decisions
+let actList = [];
+let jobList = [];
+let eduList = [];
+let homeList = [];
+let lastNames = [];
+let gender = "";
+let mNames = [];
+let fNames = [];
+let relationShips = [];// peopleList
+
+let age; // your health will start to drop when you get too old.
+let money; // money will rarely be used
+let health; // game ends if health reaches 0. Visit the hospital to maintain your health
+let looks; //
+let intelligence; // intelligence can affect job opportunities 
 let happiness;
+let name = "";
+let buttons = [];
+let menuOn = -1;
+
+// player variables speccific to items they have chosen
+let skills = [];
+let home;
+
+//helper for mouse click
+let mouseWasPressedLastFrame = false;
+
+function preload() {
+  people = loadTable("assets/people.csv", "csv", "header");
+  activities  = loadTable("assets/activities.csv", "csv", "header");
+  education = loadTable("assets/education.csv", "csv", "header");
+  jobs = loadTable("assets/jobs.csv", "csv", "header");
+  homes = loadTable("assets/homes.csv", "csv", "header");
+  
+}
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  
+  //generate family name
+  for (let r = 0; r < people.getRowCount(); r++) {
+    append(lastNames, people.getString(r, "lastNames"));
+    if (people.getString(r, "gender") == "Male") {
+      append(mNames, people.getString(r, "firstNames") );
+    }
+    if (people.getString(r, "gender") == "Female") {
+      append(fNames, people.getString(r, "firstNames") );
+    }
+  }
+  // randomizes the players gender, gives them a random last name, and a random number of siblings.
+  gender = random(["Male","Female"]);
+  let siblings = random(0,3);
+  let lastName = random(lastNames);
+  if (gender == "Male") {
+    name += random(mNames) + " " + lastName;
+  }
+  if (gender == "Female") {
+    name += random(fNames) + " " + lastName;
+  }
+  //make dad with random first name
+  append(relationShips, makePerson(int(random(25,40)), int(random(1000,100000)), int(random(10,100)), int(random(10, 100)), int(random(10, 100)), int(random(10, 100)), "Dad", random(mNames) + " " + lastName));
+  //make mom with random first name
+  append(relationShips, makePerson(int(random(25,40)), int(random(1000,100000)), int(random(10,100)), int(random(10, 100)), int(random(10, 100)), int(random(10, 100)), "Mom", random(fNames) + " " + lastName));
+  //make siblings with random genders
+  for (let i = 0; i < siblings; i++) {
+    let gen;
+    let rel;
+    if (int(random(0,2)) == 0){
+      gen = random(fNames);
+      rel = "Sister";
+    } else {
+      gen = random(mNames);
+      rel = "Brother";
+    }
+    append(relationShips, makePerson(int(random(0,5)), 0, int(random(10,100)), int(random(10, 100)), int(random(10, 100)), int(random(10, 100)), rel, gen + " " + lastName));
+  }
+  // set all other decisions
+  setDecisions();
+  
   age = 0;
   money = 0;
   health = int(random(10, 100));
   intelligence = int(random(10, 100));
   looks = int(random(10, 100));
+  happiness = int(random(10, 100));
+  buttons = [{name:"Jobs",x:(width - width/3)+55, y:(height-height/5)-10},{name:"Relationships",x:(width - width/3)+40, y:(height-height/5)+15}, {name:"Activities",x:(width - width/3)+50, y:(height-height/5)+40}, {name:"Education",x:(width - width/3)+45, y:(height-height/5)+65}, {name:"Homes",x:(width - width/3)+55, y:(height-height/5)+90}];
 }
+
+
+// setUp decisions
+function setDecisions() {
+  for (let r = 0; r < jobs.getRowCount(); r++) {
+    append(jobList, makeJob(jobs.getString(r, "Name"),int(jobs.getString(r, "Difficulty")), jobs.getString(r, "Type"),int(jobs.getString(r, "Pay"))));
+  }
+  for (let r = 0; r < activities.getRowCount(); r++) {
+    append(actList, makeActivity(activities.getString(r, "Name"),activities.getString(r, "Type")));
+  }
+  for (let r = 0; r < education.getRowCount(); r++) {
+    append(eduList, makeEducation(education.getString(r, "Name"),int(education.getString(r, "Difficulty")), education.getString(r, "Type")));
+  }
+  for (let r = 0; r < homes.getRowCount(); r++) {
+    append(homeList, makeHome(homes.getString(r, "Type"),homes.getString(r, "Properties"),int(homes.getString(r, "Cost")), int(homes.getString(r, "Quality")), false ));
+  }
+}
+
 
 function draw() {
   background(255);
   fill(220);
   rect(0,height-height/4,width,height/4);
-
   fill(0);
+  stroke(0);
+  //players stats which change troughout the game. The game ends if thhe players health reaches 0
+  text("Name: " + name, 10, height-height/5 - 15);
   text("Age: " + age, 10, height-height/5);
   text("Money: " + money, 10, (height-height/5)+15);
   text("Health: " + health, 10, (height-height/5)+30);
   text("Intelligence: " + intelligence, 10, (height-height/5)+45);
   text("Looks: " + looks, 10, (height-height/5)+60);
-  text("Click Space to age up", 10, (height-height/5)+75);
+  text("Mental Health: " + happiness, 10, (height-height/5)+75);
+  text("Click Space to age up!", 10, (height-height/5)+90);
+  stroke(10);
+  fill(220);
+  // Makes buttons where the player can make decisions
+  rect(width - width/3, height-height/5 -25, 150, 20, 20);
+  rect(width - width/3, height-height/5, 150, 20, 20);
+  rect(width - width/3, (height-height/5)+25,150, 20, 20);
+  rect(width - width/3, (height-height/5)+50, 150, 20, 20);
+  rect(width - width/3, (height-height/5)+75, 150, 20, 20);
+  fill(0);
+  for (let b of buttons) {
+    text(b.name,b.x,b.y);
+  }
+
+  
+  //This wont be in the final game but can help find precise coordinates
+  line(0,mouseY,width,mouseY);
+  line(mouseX,0,mouseX,height);
+  text(mouseX + "," + mouseY,10,10);
+  if (menuOn > -1) {
+    drawMenu(buttons[menuOn]);
+  }
+  mouseWasPressedLastFrame = mouseIsPressed;
+
 }
 
 function keyPressed() {
-  if (key === ' ' || keyCode === 65) {
+  // The player will age and a new random event will happen to them at that year
+  if ((key === ' ' || keyCode === 65) && health > 0) {
     age += 1;
-    print(age);
+    // You can have a maximum of 10 skill points. Older skills will be lost. Spec into specific skills to get job opportunities.
+    if (skills.length > 10) {
+      skills.shift();
+    }
+    if (age > 30) {
+      health -= 1;
+    }
+    if (age > 60) {
+      health -= 1;
+    }
+    print(health);
+    //print(skills);
+  }
+  
+}
+
+function mouseClicked() {
+  print(mouseX,mouseY);
+  let cX = width - width/3;
+  if (mouseX > cX && mouseX < cX + 150) { 
+    
+    if (mouseY > height-height/5 -25 && mouseY < (height-height/5 -25)+20){menuOn = 0}
+    if (mouseY > height-height/5 && mouseY < (height-height/5)+20){menuOn = 1 }
+    if (mouseY > height-height/5 +25 && mouseY < (height-height/5 +25)+20) {menuOn = 2 }
+    if (mouseY > height-height/5 +50 && mouseY < (height-height/5 +50)+20) {menuOn = 3 }
+    if (mouseY > height-height/5 +75&& mouseY < (height-height/5 +75)+20) {menuOn = 4}
+  }
+}
+
+//draws the menus for 
+function drawMenu(info) {
+  
+  stroke(10);
+  fill(220);
+  rect(150, 50, 300, 300, 20);
+  fill(0);
+  text(info.name,250,75);
+  if (menuOn == 0) { //draws menu for jobs
+    let space = 0;
+    for (let r of jobList) {
+      text(r.name + " ; " + r.type + " ; " + r.difficulty,160,100 + space);
+      fill(220);
+      rect(370,89 + space,60,12,20);
+      fill(0);
+      text("Apply", 380, 100 + space);
+      if(mouseIsPressed && (mouseX > 370 && mouseX < 370+60 && mouseY > 89 + space && mouseY < 89 + space + 12)) {
+        print(r.name);// stuff will happen here latter
+      }
+      space+=15;
+    }
+  }
+  if (menuOn == 1) { //draws menu for relationships
+    let space = 0;
+    for (let r of relationShips) {
+      text(r.name + " ; " + r.relation + " ; " + r.age,160,100 + space);
+      fill(220);
+      rect(370,89 + space,70,12,20);
+      fill(0);
+      text("Hang out", 380, 100 + space);
+      if(mouseIsPressed && (mouseX > 370 && mouseX < 370+70 && mouseY > 89 + space && mouseY < 89 + space + 12)) {
+        print(r.name);// stuff will happen here latter
+      }
+      space+=15;
+    }
+  }
+  let space = 0;
+  if (menuOn == 2) { //draws menu for activities
+    for (let r of actList) {
+      text(r.name + " ; " + r.type,160,100 + space);
+      fill(220);
+      rect(370,89 + space,40,12,20);
+      fill(0);
+      text("Join", 380, 100 + space);
+      if((mouseIsPressed && !mouseWasPressedLastFrame) && (mouseX > 370 && mouseX < 370+40 && mouseY > 89 + space && mouseY < 89 + space + 12)) {
+        print(r.name);// stuff will happen here later
+        append(skills, r.type);
+        break;
+      }
+      space+=15;
+    }
+  }
+  if (menuOn == 3) { //draws menu for education
+    for (let r of eduList) {
+      text(r.name + " ; " + r.type + " ; " + r.difficulty,160,100 + space);
+      fill(220);
+      rect(370,89 + space,40,12,20);
+      fill(0);
+      text("Join", 380, 100 + space);
+      if((mouseIsPressed && !mouseWasPressedLastFrame) && (mouseX > 370 && mouseX < 370+40 && mouseY > 89 + space && mouseY < 89 + space + 12)) {
+        print(r.name);// stuff will happen here latter
+        append(skills, r.type);
+        break;
+      }
+      space+=15;
+    }
+    
+  }
+  if (menuOn == 4) { //draws menu for homes
+    for (let r of homeList) {
+      text(r.type + " ; " + r.properties + " ; " + r.cost,160,100 + space);
+      fill(220);
+      rect(370,89 + space,40,12,20);
+      fill(0);
+      text("Rent", 380, 100 + space);
+      if(mouseIsPressed && (mouseX > 370 && mouseX < 370+40 && mouseY > 89 + space && mouseY < 89 + space + 12)) {
+        print(r.type);// stuff will happen here latter
+      }
+      space+=15;
+    }
+  }
+}
+
+
+
+
+// creates people to interact with the player
+function makePerson(age, money, health, intelligence, looks, happiness, relation, name) {
+  return {
+    age:age,
+    money:money,
+    health:health,
+    intelligence:intelligence,
+    looks:looks,
+    happiness:happiness,
+    relation:relation,
+    name:name
+  }
+}
+
+// makes homes. Some homes are free and others are rented monthly. Better homes contribute to more happiness.
+function makeHome(type, properties,cost, quality, owned) {
+  return {
+    properties:properties,
+    cost:cost,
+    type:type,
+    quality:quality,
+    owned:owned
+  }
+}
+
+// extra curricular activities. Player can gain skills ffor free to open up ob opportunities later
+function makeActivity(name, type) {
+  return {
+    name:name,
+    type:type
+  }
+}
+
+// schools and training programs. Higher difffficulty can increase your intelligence and decrease your happiness.
+function makeEducation(name, difficulty, type) {
+  return {
+    name:name,
+    difficulty:difficulty,
+    type:type
+  }
+}
+
+// when the player gains a skill through education or activities, they can get a job corresponding to it. Stats like intelligence, looks, and health, also matter.
+function makeJob(name, difficulty, type, pay) {
+  return {
+    name:name,
+    difficulty:difficulty,
+    type:type,
+    pay:pay
   }
 }
